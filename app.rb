990141373b7078ffa2a85ed309fe8ac06fc133ab
Alexandria <alex.pugia816@hotmail.com>
2019-09-16 14:57:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+        "My name is Alex"
+    end
+
+    get "/hometown" do
+        "My hometown is Milton, GA"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Unknown"
+    end
 end
